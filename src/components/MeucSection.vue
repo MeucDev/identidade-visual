3bdef894d8a821f9979f5eea181ac9b022bbcb6c
mghextreme <matiashenschel@hotmail.com>
2021-01-05 19:11:32 -0300

    Added section links.

--- src/components/MeucSection.vue
+++ src/components/MeucSection.vue
@@ -1,6 +1,6 @@
 <template>
-  <section class="section container mt-5" role="region">
-    <h2 class="mb-4">{{ title }}</h2>
+  <section :id="title" class="section container mt-5" role="region">
+    <h2 class="mb-4"><a :href="'#' + title">{{ title }}</a></h2>
     <slot></slot>
   </section>
 </template>
@@ -22,6 +22,26 @@ h2 {
   font-weight: 600;
   text-align: center;
   color: $meuc-blue;
+
+  a {
+    position: relative;
+    color: inherit;
+    text-decoration: inherit;
+
+    &:hover {
+      opacity: 0.7;
+
+      &::after {
+        content: '';
+        position: absolute;
+        top: 13px;
+        left: -30px;
+        width: 20px;
+        height: 20px;
+        background: center no-repeat url('../assets/images/link.svg');
+      }
+    }
+  }
 }
 
 p.description {
